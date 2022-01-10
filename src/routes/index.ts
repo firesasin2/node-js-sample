bfef4c918afb83ab9ee0350e425b0e2e02422c23
firesasin2 <firesasin@penta.co.kr>
2022-01-10 15:11:12 +0900

    TypeScript 형태로 전환(js -> ts)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
+import { NextFunction } from "express";
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req: any, res: any, next: any) {
   res.render('index', { title: 'Express' });
 });
 
